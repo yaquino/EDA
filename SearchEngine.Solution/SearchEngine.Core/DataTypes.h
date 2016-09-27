0e012853abe91a86e21e56b17472ab3c029532a9
ginalucia <ginaluciams@gmail.com>
2016-09-26 22:19:04 -0500

    - interface & class

--- SearchEngine.Solution/SearchEngine.Core/DataTypes.h
+++ SearchEngine.Solution/SearchEngine.Core/DataTypes.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <map>
+#include <set>
 using namespace std;
 
 // Type definitions 
+typedef bool T_BOOL;
+typedef char T_CHAR;
 typedef	long long MEMORY;
 typedef	string FILE_NAME;
 typedef	string _TOSEARCH_;
@@ -11,6 +16,12 @@ typedef	unsigned int ID_PAGE;
 typedef	unsigned int POSITION;
 typedef	unsigned int NUMBER_CHILD;
 typedef	unsigned int NUMBER_LINES;
+
+typedef ifstream READ_FILE;
+typedef ofstream WRITE_FILE;
+typedef map<int, string> URL_TABLE;
+typedef set<string> STOPWORD_TABLE;
+
 
 typedef	string WORD;
 #define SIZE 5;
--- SearchEngine.Solution/SearchEngine.Lib/CParser.h
+++ SearchEngine.Solution/SearchEngine.Lib/CParser.h
@@ -5,8 +5,21 @@
 
 class CParser : virtual public IParser {
 
+private:
+	READ_FILE file;
+	FILE_NAME file_name;
+	URL_TABLE url_table;
+	STOPWORD_TABLE stop_words_list;
+
 public:
-	CParser();
+	CParser(FILE_NAME);
+	T_BOOL LoadStopWord(FILE_NAME);
+	T_BOOL ParseFile();
+	T_BOOL IsStopWord(WORD);
+	T_BOOL IsMark(T_CHAR);
+	T_BOOL IsMark(T_CHAR);
+	T_BOOL NoMark(T_CHAR&);
+	T_BOOL PreProcessWord(WORD);
 	~CParser();
-	bool read(FILE_NAME);
+	//bool read(FILE_NAME);
 };
