f7a62c0de361aac385fa118df9951c2bef11a436
ElisbanFlores <elisban.flores@gmail.com>
2016-10-14 18:22:36 -0500

    Update CSearchEngine.cpp
    Actualizando Filtro de Urls

--- SearchEngine.Solution/SearchEngine.Lib/CSearchEngine.cpp
+++ SearchEngine.Solution/SearchEngine.Lib/CSearchEngine.cpp
@@ -128,7 +128,7 @@ T_INT CSearchEngine::funcion_Copy(FILE_NAME s1, FILE_NAME sr) {
 /**********************************************
 * FUNCION PARA OBTENER EL TAMANHO DEL ARCHIVO
 **********************************************/
-// Para determinar la el tamaño de un archivo
+// Para determinar la el tamaÃ±o de un archivo
 TIPOTAM CSearchEngine::filesize(T_CHAR archivo[])
 {
 	ifstream stream(archivo, ios::binary);
@@ -149,30 +149,31 @@ TIPOTAM CSearchEngine::filesize(T_CHAR archivo[])
 // Genera un indice a partir del archivo de las URLs
 T_INT CSearchEngine::IndexarURLs(T_CHAR *FileOrigen, T_CHAR *FileDestino)
 {
-	char *bloque, *bloqueOrigen;
-	bloque = new char[33];
-	bloqueOrigen = new char[BLOQUELINEA];
-
+	T_CHAR *bloque, *bloqueOrigen;
+	bloque = new T_CHAR[33];
+	bloqueOrigen = new T_CHAR[BLOQUELINEA];
+	
 	ifstream fOrigen(FileOrigen);
 	if (fOrigen.fail()) {
-		cerr << "Error al abrir el archivo: " << FileOrigen << endl; return 0;
+		cerr << "Error al abrir el archivo: " << FileOrigen << endl; return 0;		
 	}
 	else
 	{
-		TIPOTAM i = 0, k = 1, j = 0, l = 0, IDDoc, desde, hasta;
-		char numero[33];
-		char ch;
+		TIPOTAM i{ 0 }, k{ 1 }, j{ 0 }, l{ 0 }, IDDoc, desde, hasta;
+		T_CHAR numero[33];
+		T_CHAR ch;		
 		Indice ind;
 		ofstream fDestino(FileDestino, ios::binary);
 		if (fDestino.fail()) {
-			cerr << "Error al abrir el archivo: " << FileDestino << endl; return 0;
+			cerr << "Error al abrir el archivo: " << FileDestino << endl; return 0;			
 		}
 		ind.desde = 0;
+
 		while (!fOrigen.eof())
-		{
-			fDestino.write((char *)(&ind), sizeof(Indice));
+		{			
+			fDestino.write((T_CHAR *)(&ind), sizeof(Indice));				
 			fOrigen.getline(bloqueOrigen, 32000, '\t');
-			ind.desde = ind.desde + strlen(bloqueOrigen) + 1;
+			ind.desde = ind.desde + strlen(bloqueOrigen) + 1;		
 		}
 		fOrigen.close();
 		fDestino.close();
@@ -180,80 +181,142 @@ T_INT CSearchEngine::IndexarURLs(T_CHAR *FileOrigen, T_CHAR *FileDestino)
 	return 0;
 }
 
-//Hace la busqueda de de Documentos dentro del listado de URLs
+
+/*
+* Se carga el archivo de entrada de IDs de los que se debe retornar URLs
+*/
+
+TIPOTAM CSearchEngine::CargarIds(T_CHAR *FileIDs, TIPOTAM *&IDs)
+{
+	TIPOTAM NroIDs = 0;
+	T_CHAR *bloque;
+	bloque = new T_CHAR[33];
+	ifstream fIDs(FileIDs);
+	if (fIDs.fail()){
+		cerr << "Error al abrir el archivo: " << FileIDs << endl; return 0;
+	}
+	else{
+		while (!fIDs.eof())	{
+			fIDs.getline(bloque, 33);
+			IDs[NroIDs++] = atoll(bloque);
+		}
+		fIDs.close();		
+	}
+	return --NroIDs;
+}
+/*
+* Se carga el Indice de las URL
+*/
+TIPOTAM CSearchEngine::CargarIndexOrigen(T_CHAR *FileIndexOrigen, Indice *&ind) 
+{	
+	TIPOTAM tamIdArchivo{0};	
+	ifstream fIndexOrigen(FileIndexOrigen, ios::binary);
+	if (fIndexOrigen.fail()){
+		cerr << "Error al abrir el archivo: " << FileIndexOrigen << endl; return 0;
+	}
+	else {
+		tamIdArchivo = filesize(FileIndexOrigen) / sizeof(Indice);
+		ind = new Indice[tamIdArchivo];
+		ind[0].desde = 0;
+		fIndexOrigen.read((T_CHAR *)(ind+1), sizeof(Indice)*(tamIdArchivo));
+		fIndexOrigen.close();		
+	}
+	return tamIdArchivo;
+}
+
+/*
+* Se carga el archivo de las URL
+*/
+
+void CSearchEngine::CargarFileOrigen(ifstream &fOrigen, T_CHAR *&mOrigen, TIPOTAM desde, TIPOTAM cantidad)
+{	
+	fOrigen.seekg(desde);
+	fOrigen.read((T_CHAR *)(mOrigen), cantidad);
+	mOrigen[cantidad] = '\0';		
+}
+
+
+T_INT CSearchEngine::CargarFileOrigen_2(T_CHAR *FileOrigen, T_CHAR *&mOrigen, TIPOTAM desde,TIPOTAM cantidad)
+{
+	ifstream fOrigen(FileOrigen);
+	if (fOrigen.fail()) {
+		cerr << "Error al abrir el archivo: " << FileOrigen << endl; return 0;
+	}
+	else {
+		fOrigen.seekg(desde);
+		fOrigen.read((T_CHAR *)(mOrigen), cantidad);
+		mOrigen[cantidad] = '\0';
+		fOrigen.close();
+	}
+	return 1;
+}
+
+/*
+* Para hacer la busqueda de las URLs
+*/
 T_INT CSearchEngine::BuscarURLs(T_CHAR *FileIDs, T_CHAR *FileOrigen, T_CHAR *FileDestino, T_CHAR *FileIndexOrigen)
 {
-	char *bloque, *bloqueOrigen;
-	bloque = new char[33];
-	bloqueOrigen = new char[BLOQUELINEA];
-	ifstream fIDs(FileIDs);
-	if (fIDs.fail())
-		cerr << "Error al abrir el archivo: " << FileIDs << endl;
-	else
-	{
-		TIPOTAM i = 0, k = 1, j = 0, l = 0, IDDoc;
-		char numero[33];
-		char ch;
+	T_CHAR *bloque, *mOrigen, *mDestino;;
+	bloque = new T_CHAR[33];	
+	mOrigen = new T_CHAR[BLOQUEMEMORIA];
+	mDestino = new T_CHAR[BLOQUEMEMORIA];
 
-		// el archivo con el indice de las urls
-		ifstream fIndexOrigen(FileIndexOrigen, ios::binary);
-		if (fIndexOrigen.fail()) {
-			cerr << "Error al abrir el archivo: " << FileIndexOrigen << endl; return 0;
-		}
-		TIPOTAM tamArchivo;
-		tamArchivo = filesize(FileIndexOrigen);
-		tamArchivo = tamArchivo / sizeof(Indice);
-		cout << tamArchivo << endl;
+	TIPOTAM *IDs, restofOrigen, restofDestino;
+	IDs = new TIPOTAM[MAX_ID];
+	TIPOTAM NroIDs = 0, tamfOrigen, tamfDestino, tamIdArchivo;
 
-		// Indice en memoria
+	NroIDs = CargarIds(FileIDs,IDs);
+	
+	if (NroIDs){	
+		register TIPOTAM i{ 0 }, j{ 0 }, k{ 0 }, IDDoc;
+		TIPOTAM	tamPagina{ TPAGINA };
 		Indice *ind;
-		ind = new Indice[tamArchivo];
-		i = 0;
-		ind[i++].desde = 0;
-		while (!fIndexOrigen.eof())
-		{
-			fIndexOrigen.read((char *)(&ind[i++]), sizeof(Indice));
-		}
-		fIndexOrigen.close();
+		tamIdArchivo = CargarIndexOrigen(FileIndexOrigen, ind);
+		
+		if(tamIdArchivo){
+			tamfOrigen = filesize(FileOrigen);
+			TIPOTAM desde{ 0 }, hasta{ 0 }, puntero{ 0 };
 
-		/*cout <<"desde"<<ind[0].desde<<endl;
-		cout << "desde" << ind[1].desde << endl;
-		cout << "desde" << ind[2].desde << endl;
-		cout << "desde" << ind[3].desde << endl;
-		*/
+			ifstream fOrigen(FileOrigen);
+			if (fOrigen.fail()) {
+				cerr << "Error al abrir el archivo: " << FileOrigen << endl; return 0;
+			}
 
-		// busqueda
+			ofstream fDestino(FileDestino, ios::binary);
+			if (fDestino.fail()) {
+				cerr << "Error al abrir el archivo: " << FileDestino << endl; return 0;
+			}						
+			else{
+				T_CHAR separador = '\t';
+				
+				while (i <= NroIDs){					
+					IDDoc = IDs[(i++)-1];
+					if (IDDoc <= tamIdArchivo){
+						if (puntero <= IDDoc){
+							desde = ind[IDDoc - 1].desde;
+							puntero = IDDoc + tamPagina;
+							if (puntero <= tamIdArchivo)
+								hasta = ind[puntero - 1].desde;
+							else {
+								hasta = tamfOrigen; puntero = tamIdArchivo;
+							}
+							CargarFileOrigen(fOrigen, mOrigen, desde, hasta);	
+						}
 
-		ifstream fOrigen(FileOrigen);
-		if (fOrigen.fail()) {
-			cerr << "Error al abrir el archivo: " << FileOrigen << endl; return 0;
-		}
-		ofstream fDestino(FileDestino);
-		if (fDestino.fail()) {
-			cerr << "Error al abrir el archivo: " << FileDestino << endl; return 0;
-		}
-
-		while (!fIDs.eof())
-		{
-			fIDs.getline(bloque, 33);
-
-			IDDoc = atoll(bloque);
-			//cout << IDDoc<<endl;
-			if (IDDoc > 0)
-			{
-				//cout << "URL Encontrada"<<endl;
-				fOrigen.seekg(ind[IDDoc - 1].desde);
-				fOrigen.getline(bloqueOrigen, BLOQUELINEA, '\t');
-				fDestino.write(bloqueOrigen, strlen(bloqueOrigen) * sizeof(char));
-				fDestino.write("\n", sizeof(char));
-				//cout << bloqueOrigen << endl;
+						if (IDDoc > 0){
+							j = ind[IDDoc - 1].desde - desde;
+							while (mOrigen[j] != separador)
+								mDestino[k++] = mOrigen[j++];
+							mDestino[k++] = '\n';
+						}
+					}
+				}
+				mDestino[k] = '\0';
+				fDestino.write(mDestino, k);
+				fDestino.close();
+				fOrigen.close();
 			}
 		}
-
-		fIDs.close();
-
-		fOrigen.close();
-		fDestino.close();
-	}
+	}	
 	return 0;
 }
